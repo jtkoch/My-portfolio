70fc40ecad58b03dd918c75978639693088d7fcc
Jensen Koch <jtkoch12@gmail.com>
2020-02-11 14:24:47 -0600

    new contact form

--- contact/process.php
+++ contact/process.php
@@ -1,14 +1,22 @@
 <?php
 
-$your_email = "jtkoch12@gmail.com";
-$subject = "Portfolio Contact Message";
-$thanks_page = "thankyou.html";
+$your_email = "jtkoch12@gmail.com"; // email address to which the form data will be sent
+$subject = "Portfolio Contact Message"; // subject of the email that is sent
+$thanks_page = "contact/thankyou.html"; // path to the thank you page following successful form submission
+$contact_page = "contact/index.html"; // path to the HTML contact page where the form appears
+
+
+// Nothing needs to be modified below this line
+
+if (!isset($_POST['submit'])) {
+    header( "Location: $contact_page" );
+  }
 
 if (isset($_POST["submit"])) {
 	$nam = $_POST["name"];
 	$ema = trim($_POST["email"]);
 	$com = $_POST["comments"];
-	$loadtime = $_POST["loadtime"];
+	$spa = $_POST["spam"];
 
 	if (get_magic_quotes_gpc()) { 
 	$nam = stripslashes($nam);
@@ -26,17 +34,15 @@ if (empty($ema) || !filter_var($ema, FILTER_VALIDATE_EMAIL)) {
 	$error_msg[] = "Your email must have a valid format, such as name@mailhost.com";
 }
 
-$limit = 1000; 
+$limit = 1000;
 
 if (empty($com) || !preg_match("/^[0-9\/\-\s'\(\)!\?\.,:;\pL]+$/u", $com) || (strlen($com) > $limit)) { 
 $error_msg[] = "The Comments field must contain only letters, digits, spaces and basic punctuation (&nbsp;'&nbsp;-&nbsp;,&nbsp;.&nbsp;:&nbsp;;&nbsp;/ and parentheses), and has a limit of 1000 characters";
 }
 
-$totaltime = time() - $loadtime;
-
-if($totaltime < 7) {
-   echo("Please fill in the form before submitting!");
-   exit;
+if (!empty($spa) && !($spa == "4" || strtolower($spa) == "four")) {
+    echo "You failed the bot test!";
+    exit ();
 }
 
 // Assuming there's an error, refresh the page with error list and repeat the form
@@ -48,14 +54,15 @@ echo '<!DOCTYPE html>
 <meta charset="utf-8">
 <title>Error</title>
 <style>
-	body {background: #f7f7f7; font: 100%/1.375 georgia, serif; padding: 20px 40px;}
-	div {margin-bottom: 10px;}
+	body {background: #f7f7f7; font: 100%/1.375 georgia, serif;padding: 20px 40px;}
+	form div {margin-bottom: 10px;}
 	.content {width: 40%; margin: 0 auto;}
 	h1 {margin: 0 0 20px 0; font-size: 175%; font-family: calibri, arial, sans-serif;}
 	label {margin-bottom: 2px;}
 	input[type="text"], input[type="email"], textarea {font-size: 0.75em; width: 98%; font-family: arial; border: 1px solid #ebebeb; padding: 4px; display: block;}
 	input[type="radio"] {margin: 0 5px 0 0;}
 	textarea {overflow: auto;}
+	.hide {display: none;}
 	.err {color: red; font-size: 0.875em; margin: 1em 0; padding: 0 2em;}
 </style>
 </head>
@@ -81,7 +88,10 @@ echo '</ul>
 			<label for="comm">Comments</label>
 			<textarea name="comments" rows="10" cols="50" id="comm">'; if (isset($_POST["comments"])) {echo $com;}; echo '</textarea>
 		</div>
-		<input type="hidden" name="loadtime" value="', time(), '">
+		<div class="hide">
+			<label for="spam">What is two plus two?</label>
+			<input name="spam" type="text" size="4" id="spam">
+		</div>
 		<div>
 			<input type="submit" name="submit" value="Send">
 		</div>
@@ -99,59 +109,9 @@ $email_body =
 
 // Assuming there's no error, send the email and redirect to Thank You page
 
-if  (!$error_msg) {
+if (isset($_REQUEST['comments']) && !$error_msg) {
 mail ($your_email, $subject, $email_body, "From: $nam <$ema>" . "\r\n" . "Reply-To: $nam <$ema>" . "Content-Type: text/plain; charset=utf-8");
-
 header ("Location: $thanks_page");
 exit();
 }  
-
-} 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Contact Us</title>
-<style>
-	body {background: #f7f7f7; font: 100%/1.375 georgia, serif; padding: 20px 40px;}
-	div {margin-bottom: 10px;}
-	.content {width: 40%; margin: 0 auto;}
-	h1 {margin: 0 0 20px 0; font-size: 175%; font-family: calibri, arial, sans-serif;}
-	label {margin-bottom: 2px;}
-	input[type="text"], input[type="email"], textarea {font-size: 0.75em; width: 98%; font-family: arial; border: 1px solid #ebebeb; padding: 4px; display: block;}
-	input[type="radio"] {margin: 0 5px 0 0;}
-	textarea {overflow: auto;}
-</style>
-</head>
-
-<body>
-  <div class="content">
-	  <h1>Contact Us</h1>
-    <form method="post" action="<?php $_SERVER['PHP_SELF'] ?>">
-      <div class="row gtr-uniform">
-        <div class="col-6 col-12-xsmall">
-          <label for="name">Name</label>
-          <input name="name" type="text" id="name" placeholder="your name" value="<?php if (isset($_POST["name"])) {echo $nam;} ?>">
-        </div>
-
-        <div class="col-6 col-12-xsmall">
-          <label for="email">Email Address</label>
-          <input name="email" type="email" id="email" placeholder="your email address" value="<?php if (isset($_POST["email"])) {echo $ema;} ?>">
-        </div>
-        
-        <div class="col-12">
-          <label for="comm">Comments</label>
-          <textarea name="comments" rows="10" id="comm"><?php if (isset($_POST["comments"])) {echo $com;} ?></textarea>
-        </div>
-        
-        <input type="hidden" name="loadtime" value="<?php echo time(); ?>">
-        
-        <div>
-          <input type="submit" name="submit" value="Send">
-        </div>
-      </form>
-    </div>
-  </div>
-</body>
-</html>
+}
